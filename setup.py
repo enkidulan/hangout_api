42508f3c9b7568f0e53fb1a4dc422577e3d97187
enki <supamaxy@gmail.com>
2014-07-29 23:21:37 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+from os.path import join
+import imp
+setupy_download_helper_path = join(
+    os.path.dirname(os.path.abspath(__file__)), 'setupy_download_helper.py')
+setupy_download_helper = imp.load_source(
+    'setupy_download_helper', setupy_download_helper_path)
+
+setupy_download_helper.CHROMEDRIVER_URL_BASE = "http://chromedriver.storage.googleapis.com/%s/chromedriver_linux64.zip"
+setupy_download_helper.CHROMEDRIVER_VERSION = '2.10'
+setupy_download_helper.DEST_FILE_NAME = 'CHROMEDRIVER'
+
+version = '1.0'
+
+setup(name='hangout_api',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['hangout_api'],
+      include_package_data=True,
+      zip_safe=False,
+      cmdclass={'install': setupy_download_helper.InstallCommand,
+                'bdist_egg': setupy_download_helper.InstallCommand,
+                'develop': setupy_download_helper.DevelopCommand},
+      install_requires=[
+          'setuptools',
+          'selenium',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ setupy_download_helper.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import zipfile
+import urllib2
+import shutil
+from tempfile import NamedTemporaryFile
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from os.path import join
+# from setup import CHROMEDRIVER_VERSION
+CHROMEDRIVER_VERSION = '2.10'
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+CHROMEDRIVER_URL_BASE = "http://chromedriver.storage.googleapis.com/%s/chromedriver_linux64.zip"
+DEST_FILE_NAME = 'CHROMEDRIVER'
+
+class RequestProgressWrapper():
+    """ Simple helper for displaying file download progress;
+    if works with file-like objects"""
+    def __init__(self, obj):
+        self.obj = obj
+        self.total_size = float(obj.info().getheader('Content-Length').strip())
+        self.url = obj.url
+        self.bytes_so_far = 0
+
+    def read(self, length):
+        self.bytes_so_far += length
+        percent = self.bytes_so_far / self.total_size
+        percent = round(percent * 100, 2)
+        sys.stdout.write(
+            "%s: downloaded %d of %d bytes (%0.f%%)\r" %
+            (self.url, self.bytes_so_far, self.total_size, percent))
+        sys.stdout.flush()
+        return self.obj.read(length)
+
+    def __del__(self):
+        sys.stdout.write('\n')
+
+
+def download_ziped_resource(path, url, name, unzip=False):
+    """ files download helper """
+    full_path = join(path, name)
+    if os.path.exists(full_path):
+        return
+    req = urllib2.urlopen(url)
+    data_destination = NamedTemporaryFile() if unzip else open(full_path, 'wb')
+    with data_destination as f:
+        shutil.copyfileobj(RequestProgressWrapper(req), f)
+        if unzip:
+            f.file.seek(0)
+            zfile = zipfile.ZipFile(f.name)
+            zfile.extractall(path)
+            os.rename(os.path.join(path, zfile.namelist()[0]), full_path)
+
+
+def data_loader(command_subclass):
+    """A decorator for classes subclassing one of the setuptools commands.
+
+    It modifies the run() method so that it prints a friendly greeting.
+    """
+    orig_run = command_subclass.run
+
+    def modified_run(self):
+        orig_run(self)
+        base_path = join(self.install_lib or here, 'hangout_api')
+        self.execute(
+            download_ziped_resource,
+            (base_path,
+             CHROMEDRIVER_URL_BASE % CHROMEDRIVER_VERSION,
+             DEST_FILE_NAME,
+             True),
+            msg="Downloading %s" % DEST_FILE_NAME)
+    command_subclass.run = modified_run
+    return command_subclass
+
+
+@data_loader
+class DevelopCommand(develop):
+    pass
+
+
+@data_loader
+class InstallCommand(install):
+    pass
